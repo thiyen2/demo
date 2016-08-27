fe0863218a448266ca75ef1defb24e05ece51442
thiyen2 <yenntph04028@fpt.edu.vn>
2016-08-27 13:59:31 +0700

    ulaod file

---
+++ Backup batabase.sql
@@ -0,0 +1,37 @@
+
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Lun 06 Avril 2015 à 12:06
+-- Version du serveur: 10.0.12-MariaDB
+-- Version de PHP: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `u993083976_use`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `usse`
+--
+
+CREATE TABLE IF NOT EXISTS `usse` (
+  `ID` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `Full Name` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
